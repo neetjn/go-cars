e9d082c4618245262bd008f5b749193c048655e5
John Nolette <jnolette@transparent.com>
2018-10-17 20:46:34 -0400

    Adding capability to create resources, using http constants

--- cars.go
+++ cars.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	// "os"
 	"io/ioutil"
 	"net/http"
 
@@ -12,35 +11,6 @@ import (
 )
 
 func main() {
-
-	// go imports all files in the same directory!!!
-	// the main func is whats executed :)
-
-	// car := app.CarDto{
-	// 	Make:  "Hyundia",
-	// 	Model: "Elantra",
-	// 	Vin:   "10921341Q41",
-	// 	Year:  2010,
-	// 	Miles: 76100,
-	// }
-	// b, _ := json.Marshal(car)
-	// fmt.Println(string(b))
-
-	// embedded structs must be initialized by their field name
-	// as composite literals
-
-	// truck := app.TruckDto{
-	// 	CarDto: app.CarDto{
-	// 		Make:  "Toyota",
-	// 		Model: "Explorer",
-	// 		Vin:   "43133409A53100",
-	// 		Year:  1999,
-	// 		Miles: 146100,
-	// 	},
-	// 	Height: 7.1,
-	// }
-	// b, _ = json.Marshal(truck)
-	// fmt.Println(string(b))
 
 	data, _ := ioutil.ReadFile("data/cars.json")
 	cars := core.CarCollectionDto{}
@@ -58,7 +28,7 @@ func main() {
     processed, _ := json.Marshal(sd)
 
     switch r.Method {
-    case "GET":
+    case http.MethodGet:
       w.Header().Set("Content-Type", "application/json")
       fmt.Fprintf(w, string(processed))
     }
@@ -66,40 +36,54 @@ func main() {
 
   core.AddResource("/cars/", "cars-collection", "", func(w http.ResponseWriter, r *http.Request) {
     switch r.Method {
-    case "GET":
+    case http.MethodGet:
       resp, _ := json.Marshal(cars)
       w.Header().Set("Content-Type", "application/json")
       fmt.Fprintf(w, string(resp))
-    case "POST":
-      // TODO: left here, figure out how to read request body
-      w.Header().Set("Content-Type", "application/json")
-      fmt.Fprintf(w, "[]")
+    case http.MethodPost:
+      body, _ := ioutil.ReadAll(r.Body)
+      defer r.Body.Close()
+      car := core.CarDto{}
+      err := json.Unmarshal(body, &car)
+      if err != nil {
+        cars.Items = append(cars.Items, car)
+        w.WriteHeader(http.StatusCreated)
+      } else {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+      }
     }
   })
 
   core.AddResource("/trucks/", "trucks-collection", "", func(w http.ResponseWriter, r *http.Request) {
     switch r.Method {
-    case "GET":
+    case http.MethodGet:
       resp, _ := json.Marshal(trucks)
       w.Header().Set("Content-Type", "application/json")
       fmt.Fprintf(w, string(resp))
-    case "POST":
-      // TODO: left here, figure out how to read request body
-      w.Header().Set("Content-Type", "application/json")
-      fmt.Fprintf(w, "[]")
+    case http.MethodPost:
+      body, _ := ioutil.ReadAll(r.Body)
+      defer r.Body.Close()
+      truck := core.TruckDto{}
+      err := json.Unmarshal(body, &truck)
+      if err != nil {
+        trucks.Items = append(trucks.Items, truck)
+        w.WriteHeader(http.StatusCreated)
+      } else {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+      }
     }
   })
 
   core.AddResource("/vehicles/", "vehicles-collection", "", func(w http.ResponseWriter, r *http.Request) {
     switch r.Method {
-    case "GET":
+    case http.MethodGet:
       resp, _ := json.Marshal(core.VehicleCollectionDto{
         Cars:   cars,
         Trucks: trucks,
       })
       w.Header().Set("Content-Type", "application/json")
       fmt.Fprintf(w, string(resp))
-    case "POST":
+    case http.MethodPost:
       // TODO: left here, figure out how to read request body
       w.Header().Set("Content-Type", "application/json")
       fmt.Fprintf(w, "[]")
